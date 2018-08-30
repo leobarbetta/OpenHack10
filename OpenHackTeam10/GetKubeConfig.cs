01fcf609288df25391979c87b109900bd2e1f247
Leonardo Barbetta <leo_barbetta@outlook.com>
2018-08-30 15:13:53 -0300

    docker

--- OpenHackTeam10/GetKubeConfig.cs
+++ OpenHackTeam10/GetKubeConfig.cs
@@ -11,37 +11,37 @@ namespace OpenHackTeam10
     {
         public static IKubernetes GetKubernetes()
         {
-            //string clientId = "bf36d86f-09b9-48ef-8c69-834ac7fcf3c6";
-            //string clientSecret = "8edc77a2-02f9-4cc6-9fe5-3fc85a83eae9";
-            //string tenantId = "1e6debca-08b7-4ccb-92f0-7a73e2f4da68";
+            string clientId = "bf36d86f-09b9-48ef-8c69-834ac7fcf3c6";
+            string clientSecret = "8edc77a2-02f9-4cc6-9fe5-3fc85a83eae9";
+            string tenantId = "1e6debca-08b7-4ccb-92f0-7a73e2f4da68";
 
-            //string resourceGroupName = "OpenHackTeam10";
-            //string clusterName = "minecraft-cluster-leonardo";
+            string resourceGroupName = "OpenHackTeam10";
+            string clusterName = "minecraft-cluster-leonardo";
 
-            //var credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(
-            //        clientId,
-            //        clientSecret,
-            //        tenantId,
-            //        AzureEnvironment.AzureGlobalCloud);
+            var credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(
+                    clientId,
+                    clientSecret,
+                    tenantId,
+                    AzureEnvironment.AzureGlobalCloud);
 
-            //var azure = Azure
-            //             .Configure()
-            //             .Authenticate(credentials)
-            //             .WithDefaultSubscription();
+            var azure = Azure
+                         .Configure()
+                         .Authenticate(credentials)
+                         .WithDefaultSubscription();
 
-            //IKubernetesCluster kubernetesCluster = azure.KubernetesClusters.GetByResourceGroup(resourceGroupName, clusterName);
+            IKubernetesCluster kubernetesCluster = azure.KubernetesClusters.GetByResourceGroup(resourceGroupName, clusterName);
 
-            //var buffer = kubernetesCluster.UserKubeConfigContent;
-            //var configFile = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+            var buffer = kubernetesCluster.UserKubeConfigContent;
+            var configFile = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
 
 
-            //var streamConfig = new MemoryStream();
-            //var writer = new StreamWriter(streamConfig);
-            //writer.Write(configFile);
-            //writer.Flush();
-            //streamConfig.Position = 0;
+            var streamConfig = new MemoryStream();
+            var writer = new StreamWriter(streamConfig);
+            writer.Write(configFile);
+            writer.Flush();
+            streamConfig.Position = 0;
 
-            FileInfo configFile = new FileInfo(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\config"));
+            //FileInfo configFile = new FileInfo(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\config"));
 
             var config = KubernetesClientConfiguration.BuildConfigFromConfigFile(configFile);
             return new Kubernetes(config);
