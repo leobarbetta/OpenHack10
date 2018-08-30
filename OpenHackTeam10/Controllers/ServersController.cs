1bb3e17756c590dbf98442cf0a84d8394a18638c
Leonardo Barbetta <leo_barbetta@outlook.com>
2018-08-30 12:22:38 -0300

    Add servers

--- OpenHackTeam10/Controllers/ServersController.cs
+++ OpenHackTeam10/Controllers/ServersController.cs
@@ -1,5 +1,7 @@
 ﻿using k8s;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OpenHackTeam10.Controllers
 {
@@ -13,9 +15,44 @@ namespace OpenHackTeam10.Controllers
             var config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
             IKubernetes client = new Kubernetes(config);
 
-            var servers = client.ListAPIService1();
+            var serversRest = client.ListNamespacedService("default");
+
+            List<Server> servers = new List<Server>();
+
+            foreach (var item in serversRest.Items)
+            {
+                Server server = new Server
+                {
+                    Name = item.Metadata.Name,
+                    IP = item.Status?.LoadBalancer?.Ingress?.FirstOrDefault()?.Ip
+                };
+
+                foreach (var port in item.Spec.Ports)
+                {
+                    server.Ports.Add(new Port
+                    {
+                        Name = port.Name,
+                        PortNumber = port.Port.ToString()
+                    });
+                }
+
+                servers.Add(server);
+            }
 
             return Ok(servers);
         }
+    }
+
+    public class Server
+    {
+        public string Name { get; set; }
+        public string IP { get; set; }
+        public List<Port> Ports { get; set; } = new List<Port>();
+    }
+
+    public class Port
+    {
+        public string Name { get; set; }
+        public string PortNumber { get; set; }
     }
 }
